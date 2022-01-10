9f99c4de25c03c4edd03ef346c4163d341d09d2d
mincastle98 <alstjd98522@naver.com>
2022-01-10 20:48:41 +0900

    회원 관리 예제

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;      // 반환 객체가 없을 때 null로 반환하는 데 이를 Optional로 감싸서 반환함?
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
